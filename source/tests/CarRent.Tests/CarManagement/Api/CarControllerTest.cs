0b34ef69e1848986ae49d73436b789679c232ccf
ragi <ragi>
2021-08-20 16:32:29 +0200

    Added CarServiceFace and added first Tests for the Controller

---
+++ source/tests/CarRent.Tests/CarManagement/Api/CarControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRent.CarManagment.Application;
+using CarRent.Common.Application;
+using CarRent.Controllers;
+using CarRent.Tests.CarManagement.Application;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace CarRent.Tests.CarManagement.Api
+{
+    public class CarControllerTest
+    {
+        CarController _controller;
+        ICarService _service;
+
+        public CarControllerTest()
+        {
+            _service = new CarServiceFake();
+            _controller = new CarController(_service);
+        }
+
+
+        [Test]
+        public void Get_FindAll_ReturnsOkResult()
+        {
+            // Arrange
+            var result = _controller.Get();
+            // Assert
+            Assert.IsInstanceOf<OkObjectResult>(result.Result);
+        }
+
+        [Test]
+        public void Get_WhenCalled_ReturnsAllItems()
+        {
+            // Arrange
+            var okResult = _controller.Get().Result as OkObjectResult;
+            // Act
+            var items = (ServiceResponse<IEnumerable<GetCarDto>>) okResult.Value;
+            // Assert
+            Assert.AreEqual(7, items.Data.Count());
+        }
+    }
+}
---
+++ source/tests/CarRent.Tests/CarManagement/Application/CarServiceFake.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRent.CarManagment.Application;
+using CarRent.CarManagment.Domain;
+using CarRent.Common.Application;
+using MongoDB.Bson;
+
+namespace CarRent.Tests.CarManagement.Application
+{
+    class CarServiceFake : ICarService
+    {
+        private readonly List<GetCarDto> _car;
+        public CarServiceFake()
+        {
+            _car = new List<GetCarDto>()
+            {
+                new GetCarDto() {Id = "asdasdasdasdasd1", Name = "Fiat 500"},
+                new GetCarDto() {Id = "asdasdasdasdasd2", Name = "Ferrari F50"},
+                new GetCarDto() {Id = "asdasdasdasdasd3", Name = "Lamborghini Urus"},
+                new GetCarDto() {Id = "asdasdasdasdasd4", Name = "Koenigsegg All"},
+                new GetCarDto() {Id = "asdasdasdasdasd5", Name = "Fiat Panda"},
+                new GetCarDto() {Id = "asdasdasdasdasd6", Name = "Smart"},
+                new GetCarDto() {Id = "asdasdasdasdasd7", Name = "Jeep Wrangler"},
+            };
+        }
+
+        public Task<ServiceResponse<GetCarDto>> AddCar(AddCarDto carDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<GetCarDto>> FindOneById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse<IEnumerable<GetCarDto>>> FindAll()
+        {
+            ServiceResponse<IEnumerable<GetCarDto>> serviceResponse = new ServiceResponse<IEnumerable<GetCarDto>>();
+            serviceResponse.Data = _car.AsEnumerable<GetCarDto>();
+            return Task<ServiceResponse<IEnumerable<GetCarDto>>>.FromResult(serviceResponse);
+
+        }
+        public Task<ServiceResponse<GetCarDto>> Update(GetCarDto carDto)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<ServiceResponse<IEnumerable<GetCarDto>>> DeleteById(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
