30d5615327935f3adbfaca1913e4407f52fdb731
ragi <ragi>
2021-08-20 10:41:03 +0200

    Car is now a Document

---
+++ source/src/CarRent/CarManagment/Domain/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarRent.Common.Domain;
+
+namespace CarRent.CarManagment.Domain
+{
+    [BsonCollection("car")]
+    public class Car : Document
+    {
+        public string name { get; set; }
+    }
+}
