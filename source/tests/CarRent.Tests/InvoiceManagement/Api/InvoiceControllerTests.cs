dae5927b281a61175e8b242cd6c3a5d4c809ed76
ragi <ragi>
2021-08-27 21:55:49 +0200

    added invoice test

---
+++ source/tests/CarRent.Tests/InvoiceManagement/Api/InvoiceControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CarRent.Common.Application;
+using CarRent.CustomerManagement.Application.Dto;
+using CarRent.InvoiceManagement.Api;
+using CarRent.InvoiceManagement.Application;
+using CarRent.InvoiceManagement.Application.Dto;
+using CarRent.ReservationManagement.Api;
+using CarRent.ReservationManagement.Application;
+using CarRent.ReservationManagement.Application.Dto;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace CarRent.Tests.InvoiceManagement.Api
+{
+    public class InvoiceControllerTests
+    {
+        private readonly InvoiceController _controller;
+        private readonly IInvoiceService _service;
+
+        public InvoiceControllerTests()
+        {
+            _service = A.Fake<IInvoiceService>();
+            _controller = new InvoiceController(_service);
+        }
+
+        [Fact]
+        public void Get_FindAll_ReturnsOkResult()
+        {
+            // Arrange
+            var result = _controller.Get();
+            // Assert
+            Assert.IsType<ActionResult<ServiceResponse<List<GetInvoiceDto>>>>(result.Result);
+        }
+
+        [Fact]
+        public void Get_WhenCalled_ReturnsTaskIActionResult()
+        {
+            // Arrange
+            var task = A.Fake<Task<ServiceResponse<IEnumerable<GetInvoiceDto>>>>();
+            // Act
+            var result = _controller.Get();
+            A.CallTo(() => _service.FindAll()).MustHaveHappened();
+            A.CallTo(() => _service.FindAll()).Equals(task);
+
+            // Assert
+            Assert.IsType<ActionResult<ServiceResponse<List<GetInvoiceDto>>>>(result.Result);
+        }
+    }
+}
---
+++ source/tests/CarRent.Tests/InvoiceManagement/Application/InvoiceServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using CarRent.CarManagement.Domain;
+using CarRent.Common.Application;
+using CarRent.Common.Infrastructure;
+using CarRent.InvoiceManagement.Application;
+using CarRent.InvoiceManagement.Application.Dto;
+using CarRent.InvoiceManagement.Application.Mapper;
+using CarRent.InvoiceManagement.Domain;
+using CarRent.ReservationManagement.Application;
+using CarRent.ReservationManagement.Application.Dto;
+using CarRent.ReservationManagement.Application.Mapper;
+using CarRent.ReservationManagement.Domain;
+using FakeItEasy;
+using Xunit;
+
+namespace CarRent.Tests.InvoiceManagement.Application
+{
+    public class InvoiceServiceTests
+    {
+        private readonly IInvoiceMapper _ownMapper;
+        private readonly IMongoRepository<Invoice> _repo;
+        private readonly IMongoRepository<Car> _carRepo;
+        private readonly IMongoRepository<Reservation> _reservationRepo;
+        private readonly IInvoiceService _service;
+
+        public InvoiceServiceTests()
+        {
+            _repo = A.Fake<IMongoRepository<Invoice>>();
+            _carRepo = A.Fake<IMongoRepository<Car>>();
+            _reservationRepo = A.Fake<IMongoRepository<Reservation>>();
+            _ownMapper = A.Fake<InvoiceMapper>();
+            _service = new InvoiceService(_repo, _reservationRepo, _carRepo, _ownMapper);
+        }
+    }
+}
